81ca567cdbe753a8eb029ae9d5fd65407495d2a0
suhasini <suhasinishinde49@gmail.com>
2024-03-30 18:25:56 +0530

    Latest code3

---
+++ src/test/java/listeners/AllureReportListener.java
@@ -0,0 +1,18 @@
+/**
+ * This class will add screenshot of failed testcases into an Allure Report.
+ */
+package listeners;
+
+import org.testng.ITestListener;
+import org.testng.ITestResult;
+
+/**
+ * @author Suhasini Shinde.
+ */
+public class AllureReportListener implements ITestListener {
+	
+	@Override
+	public void onTestFailure(ITestResult result) {
+	}
+
+}
